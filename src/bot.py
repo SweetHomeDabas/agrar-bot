d345612b30cdf78925a6f839b1c79d491532c362
SweetHomeDabas <93444519+SweetHomeDabas@users.noreply.github.com>
2026-05-12 11:37:02 +0200

    bot.py

--- src/bot.py
+++ src/bot.py
@@ -74,6 +74,7 @@ async def check_price_alerts(notifier: Notifier, price_monitor: PriceMonitor,
 
 
 async def main():
+    start_health_server()
     telegram_token = os.environ["TELEGRAM_BOT_TOKEN"]
     chat_id = os.environ["TELEGRAM_CHAT_ID"]
     anthropic_key = os.environ["ANTHROPIC_API_KEY"]
